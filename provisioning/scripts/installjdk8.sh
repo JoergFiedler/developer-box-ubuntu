fa67a36507f626947d0d6c830c791176c3152e4b
JoergFiedler <joerg.fiedler@moumantai.de>
2014-06-19 23:22:40 +0200

    introduce roles

--- provisioning/scripts/installjdk8.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections
--- provisioning/scripts/installnode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | sh creates=~/.nvm
-. .profile
-nvm install 0.10
-echo 'nvm use 0.10' >> ~/.profile
-
